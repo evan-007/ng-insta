6449f59e4f58855e5ad366f4db3212e730d53f42
Evan Lloyd <eulloyd@gmail.com>
2014-05-27 19:14:42 +0900

    complete basic search functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 angular.module('instaApp', [])
+.config(function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+})
 .controller('instaCtrl', function($scope, $http){
-  $scope.getResults = function(){
-    
-  };
+  $scope.getResults = function(query){
+		$scope.query = query;
+		$http({
+			method: 'JSONP',
+			url: 'https://api.instagram.com/v1/tags/'+query+'/media/recent',
+			params: {
+				client_id: '6228b1584b8045269663b24eb1d03ae1',
+				callback: 'JSON_CALLBACK'
+			}
+		})
+		.success(function(data, status, headers, config) {
+			console.log(data);
+			$scope.images = data.data;
+			$scope.count = data.data.length;
+		});
+	};
   $scope.images = '';
 });
